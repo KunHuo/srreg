03a1e15678022cdd2ec2c6f0d404f59d71561358
KunHuo <610112077@qq.com>
2023-02-21 12:04:23 +0800

    Update associate.R

--- R/associate.R
+++ R/associate.R
@@ -6,8 +6,8 @@ associate <- function(data,
                       args = list(),
                       p.trend = TRUE,
                       n.quantile = NULL,
-                      right = TRUE,
-                      labels = NULL,
+                      quantile.right = TRUE,
+                      quantile.labels = NULL,
                       ...){
 
   outcome    <- srmisc::select_variable(data, outcome)
@@ -49,7 +49,7 @@ associate <- function(data,
     srmisc::typeset(fit, data = data, outcome = outcome, varnames = x, filter = x,  ...)
   }
 
-  # Cyclic execution of regression models
+
   # If the exposed variable is continuous, the exposed variable is grouped
   # according to the quantile specified by n.quantile.
   exec <- function(covar){
@@ -57,22 +57,26 @@ associate <- function(data,
       data <- srmisc::cut_quantile(data,
                                    varname = exposure,
                                    n = n.quantile,
-                                   right = right,
-                                   labels = labels)
-
+                                   right = quantile.right,
+                                   labels = quantile.labels)
       # Model for quantiles.
       res1 <- model(data, x = paste0("gq_", exposure), covar)
-
       if(p.trend){
         # Model for trend.
         res2 <- model(data, x = paste0("mq_", exposure), covar)
         res2 <- res2[, c(1, ncol(res2)), drop = FALSE]
-        res1[1, 1] <- exposure
-        res2[1, 1] <- exposure
-        names(res2)[2] <- "P for trend"
+        res1[1, 1] <- srmisc::get_var_label(data, exposure, default = ".name")
+        res2[1, 1] <- srmisc::get_var_label(data, exposure, default = ".name")
+
+        if(length(covariates) == 1L){
+          names(res2)[2] <- "P for trend a"
+        }else{
+          names(res2)[2] <- sprintf("P for trend %s", letters[length(covariates) + 1])
+        }
+
         srmisc::merge_left(res1, res2, by = names(res1)[1])
       }else{
-        res1[1, 1] <- exposure
+        res1[1, 1] <- srmisc::get_var_label(data, exposure, default = ".name")
         res1
       }
     }else{
@@ -80,29 +84,72 @@ associate <- function(data,
     }
   }
 
+  # Cyclic execution of regression models
   results <- lapply(covariates, exec)
+
+  # Output
   output <- results[[1]]
 
   if(length(results) > 1L){
-    MNAMES <-sprintf("Model %d %s", 1:length(output), letters[1:length(output)])
+    # Model names
+    if(srmisc::is_empty(names(results))){
+      MNAMES <-sprintf("Model %d %s", 1:length(results), letters[1:length(results)])
+    }else{
+      MNAMES <- names(results)
+      MNAMES <-sprintf("%s %s", MNAMES, letters[1:length(results)])
+    }
     names(output) <- c(names(output)[1:2], paste(MNAMES[1], names(output)[-c(1, 2)], sep = "__"))
 
+    # Merge model results bt variable
     for(i in 2:length(results)){
       output <- srmisc::merge_table(output, results[[i]][, -2, drop = FALSE], name.y = MNAMES[i])
     }
-
-    notes <- sapply(covariates, function(x){
-      if(is.null(x)){
-        "Adjusted for nothing."
-      }else{
-        paste("Adjusted for", paste(x, sep = ", "), sep = " ")
-      }
-    })
-  }else{
-    notes <- "ddd"
   }
 
+  # Output title
+  if(is.null(time)){
+    if(length(unique(data[[outcome]])) == 2L){
+      title <- "logistic"
+    }else{
+      title <- "linear"
+    }
+  }else{
+    title <- "Cox"
+  }
 
-  attr(output, "note") <- notes
+  # Output notes
+  notes <- sapply(covariates, function(x){
+    if(srmisc::is_empty(x)){
+      "Adjusted for nonthing."
+    }else{
+      sprintf("Adjusted for %s.", paste(x, collapse = ", "))
+    }
+  })
+  if(length(notes) == 1L){
+    notes <- notes[[1]]
+  }else{
+    notes <- sprintf("%s %s", letters[1:length(notes)], notes)
+    notes <- paste(notes, collapse = "\n")
+  }
+
+  # Note of p for trend.
+  if(!is.null(n.quantile) & p.trend){
+    if(n.quantile == 3L){
+      note.trend <- "Tests for linear trend were done by modeling the median value of each tertile to test ordered relations across tertiles of %s"
+    }else if(n.quantile == 4L){
+      note.trend <- "Tests for linear trend were done by modeling the median value of each quantile to test ordered relations across quantiles of %s"
+    }else{
+      note.trend <- "Tests for linear trend were done by modeling the median value of each group to test ordered relations across quantiles of %s"
+    }
+    note.trend <- sprintf(note.trend, srmisc::get_var_label(data, exposure, default = ".name"))
+    if(length(results) == 1L){
+      notes <- c(notes, sprintf("\n%s %s.", "a", note.trend))
+    }else{
+      notes <- c(notes, sprintf("\n%s %s.", letters[length(results) + 1], note.trend))
+    }
+  }
+
+  attr(output, "title") <- title
+  attr(output, "note")  <- notes
   output
 }
