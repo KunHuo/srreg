21645ed1c5b494620c0895c071a3aa1733a9a002
KunHuo <610112077@qq.com>
2023-02-22 12:00:41 +0800

    Update subgroup.R

--- R/subgroup.R
+++ R/subgroup.R
@@ -11,14 +11,15 @@ subgroup <- function(data,
   exposure   <- srmisc::select_variable(data, exposure)
   covariates <- srmisc::select_variable(data, covariates)
   strata     <- srmisc::select_variable(data, strata)
+
   if(srmisc::is_empty(strata)){
     strata <- setdiff(names(data), outcome)
     strata <- setdiff(strata, time)
     strata <- setdiff(strata, exposure)
   }
   strata <- srmisc::select_factor(data, varnames = strata)
-
-  srmisc::fmt_reg(data = data, varnames = strata)
+  strata <- setdiff(strata, outcome)
+  strata <- setdiff(strata, exposure)
 
   # P.LRT <- lapply(strata, function(sg){
   #   adjusted <- setdiff(adjusted, sg)
@@ -36,34 +37,53 @@ subgroup <- function(data,
   # })
 
   exec2 <- function(svar){
-    gres <- group_exec(data, group = svar, func = function(d){
+    gres <-  srmisc::group_exec(data, group = svar, func = function(d){
       d <-  droplevels.data.frame(d)
       res <- associate(data = d,
                 outcome = outcome,
                 time = time,
                 exposure = exposure,
                 covariates = setdiff(covariates, svar),
-                select = c("effect", "p"))
+                select = c("effect", "p"), ...)
+      if(!is.numeric(data[[exposure]])){
+        res <- res[-c(1, 2), , drop = FALSE]
+      }
       res
-    })
-    # names(gres)[1] <- "subgroup"
+    }, warning = FALSE)
+    gres <- gres[, -2, drop = FALSE]
     gres
   }
 
   exec3 <- function(svar){
-    gres <- group_exec(data, group = svar, func = function(d){
+    gres <- srmisc::group_exec(data, group = svar, func = function(d){
       d <-  droplevels.data.frame(d)
       res <- associate(data = d,
                        outcome = outcome,
                        time = time,
                        exposure = exposure,
                        covariates = setdiff(covariates, svar),
-                       select = c("effect", "p"))
+                       select = c("effect"), ...)
+      res <- res[-1, , drop = FALSE]
       res
     })
+
+    gres$id <- rep(1:length(unique(data[[svar]])), each = length(unique(data[[exposure]])))
+
+    gres <- srmisc::reshape_wide(gres, id = "id", names.from = 2, values.from = 3, include.id = FALSE)
     gres
   }
 
-  lapply(strata, exec2)
+  output <- lapply(strata, exec3)
+
+  output <- lapply(output, function(x){
+    x[[1]] <- paste(names(x)[1], x[[1]], sep = "")
+    names(x)[1] <- "term"
+    x
+  })
+  output <- do.call(rbind, output)
+  output <- srmisc::merge_left(srmisc::fmt_reg(data = data, varnames = strata), output, by = "term")
+  output <- output[, -c(1:3), drop = FALSE]
+  names(output)[1] <- "Subgroup"
+  output
 }
 
