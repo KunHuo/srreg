0ffc47a854e70a750c367843da3df7936cc53a04
KunHuo <610112077@qq.com>
2023-02-23 11:28:07 +0800

    Create print.R

---
+++ R/print.R
@@ -0,0 +1,23 @@
+#' Print srreg
+#'
+#' @param x a srreg object
+#' @param ... unused.
+#'
+#' @keywords internal
+#' @export
+print.srreg <- function(x, ...){
+  # if("P value" %in% names(x)){
+  #   x[["P value"]] <- srmisc::fmt_signif(x[["P value"]])
+  # }
+  #
+  # STAT.NAME <- c("t value", "Std. error", "Wald", "B", "\u03b2")
+  #
+  # if(any(STAT.NAME %in% names(x))){
+  #   STAT.NAME <- STAT.NAME[STAT.NAME %in% names(x)]
+  #
+  #   x[STAT.NAME] <- lapply(x[STAT.NAME], function(i){
+  #     ifelse(is.na(i), NA, format(i, justify = "right"))
+  #   })
+  # }
+  srmisc::print_booktabs(x, ...)
+}
