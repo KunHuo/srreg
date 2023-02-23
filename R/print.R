642019cb17f10b214570f671701ad1690691b2ed
KunHuo <610112077@qq.com>
2023-02-23 16:50:56 +0800

    Update print.R

--- R/print.R
+++ R/print.R
@@ -6,18 +6,18 @@
 #' @keywords internal
 #' @export
 print.srreg <- function(x, ...){
-  # if("P value" %in% names(x)){
-  #   x[["P value"]] <- srmisc::fmt_signif(x[["P value"]])
-  # }
-  #
-  # STAT.NAME <- c("t value", "Std. error", "Wald", "B", "\u03b2")
-  #
-  # if(any(STAT.NAME %in% names(x))){
-  #   STAT.NAME <- STAT.NAME[STAT.NAME %in% names(x)]
-  #
-  #   x[STAT.NAME] <- lapply(x[STAT.NAME], function(i){
-  #     ifelse(is.na(i), NA, format(i, justify = "right"))
-  #   })
-  # }
+  if("P value" %in% names(x)){
+    x[["P value"]] <- srmisc::fmt_signif(x[["P value"]])
+  }
+
+  STAT.NAME <- c("t value", "Std. error", "Wald", "B", "\u03b2")
+
+  if(any(STAT.NAME %in% names(x))){
+    STAT.NAME <- STAT.NAME[STAT.NAME %in% names(x)]
+
+    x[STAT.NAME] <- lapply(x[STAT.NAME], function(i){
+      ifelse(is.na(i), NA, format(i, justify = "right"))
+    })
+  }
   srmisc::print_booktabs(x, ...)
 }
