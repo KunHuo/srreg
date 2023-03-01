79f8bac87239b555c549afc6579411dcc9a6778c
KunHuo <610112077@qq.com>
2023-03-01 22:15:18 +0800

    Update utils.R

--- R/utils.R
+++ R/utils.R
@@ -84,13 +84,19 @@ create_formula <- function(dependent, independents = NULL){
   if(length(dependent) == 2L){
     dependent <- sprintf("survival::Surv (%s, %s)", dependent[1], dependent[2]) # time, status
   }
-  if(length(independents) == 1L){
-    frm <- paste(dependent, independents, sep = " ~ ")
+
+  if(is.null(independents)){
+    frm <- paste(dependent, "1", sep = " ~ ")
     frm <- stats::as.formula(frm)
   }else{
-    frm <- paste(independents, collapse = " + ")
-    frm <- paste(dependent, frm, sep = " ~ ")
-    frm <- stats::as.formula(frm)
+    if(length(independents) == 1L){
+      frm <- paste(dependent, independents, sep = " ~ ")
+      frm <- stats::as.formula(frm)
+    }else{
+      frm <- paste(independents, collapse = " + ")
+      frm <- paste(dependent, frm, sep = " ~ ")
+      frm <- stats::as.formula(frm)
+    }
   }
   frm
 }
