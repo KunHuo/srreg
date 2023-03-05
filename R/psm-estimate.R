b6d1bf80a181d1a0f0864efd85ce68aeb4f74119
KunHuo <610112077@qq.com>
2023-03-05 10:56:48 +0800

    Update psm-estimate.R

--- R/psm-estimate.R
+++ R/psm-estimate.R
@@ -14,40 +14,65 @@ psm_estimate <- function(object, model = "auto", ...){
   positive   <- object$positive
   covariates <- object$covariates
 
+  model <- auto_model(object$data, outcome, time, model)
+
   res.adj <- associate(data = object$data,
             outcome = outcome,
             time = time,
             exposure = exposure,
             positive = positive,
             covariates = covariates,
+            model = model,
             ...)
+  res.adj[1, 1] <- "Adjusted model"
 
-
-  model <- auto_model(object$data, outcome, time, model)
   frm <- create_formula(c(time, outcome), c(exposure))
 
-
   data.psm <- psm_data(object)
-  class(data.psm) <- "data.frame"
-  if(model == "linear"){
-    fit.psm <- srmisc::do_call(model,
-                           data = data.psm,
-                           formula = frm,
-                           weights = data.psm$weights)
+
+  if(model == "cox"){
+
   }else{
-    fit.psm <- srmisc::do_call(model,
-                           data = data.psm,
-                           formula = frm,
-                           positive = positive,
-                           weights = data.psm$weights)
+    if(model == "linear"){
+      fit.psm <- srmisc::do_call(model,
+                                 data = data.psm,
+                                 formula = frm,
+                                 weights = data.psm$weights)
+      exp <- FALSE
+    }else{
+      fit.psm <- srmisc::do_call(model,
+                                 data = data.psm,
+                                 formula = frm,
+                                 positive = positive,
+                                 weights = data.psm$weights)
+      exp <- TRUE
+    }
+    res.psm <- lmtest::coeftest(fit.psm,
+                                vcov. = sandwich::vcovCL,
+                                cluster = data.psm$subclass)
+    res.psm <- srmisc::typeset(res.psm,
+                               data = data.psm,
+                               outcome = outcome,
+                               varnames = exposure,
+                               filter = exposure,
+                               exp = exp)
+    res.psm[1, 1] <- "Propensity matching"
+    names(res.psm) <- names(res.adj)
   }
-  res.psm <- lmtest::coeftest(fit.psm, vcov. = sandwich::vcovCL, cluster = data.psm$subclass)
-  res.psm
 
-  # data.iptw <- psm_data(object, type = "iptw")
-  #
-  # iptw_model(data = data.iptw, frm = frm, model = model)
-
+  data.iptw <- psm_data(object, type = "iptw")
+  data.iptw <- positive_event(data = data.iptw, outcome = outcome, positive)
+  res.iptw <- iptw_model(data = data.iptw, frm = frm, model = model)
+  res.iptw <- typeset(res.iptw,
+                      data = data.iptw,
+                      outcome = outcome,
+                      varnames = exposure,
+                      filter = exposure)
+  res.iptw
+  # res.iptw[1, 1] <- "IPTW"
+  # out <- rbind(res.adj, res.psm)
+  # out <- rbind(out, res.iptw)
+  # out
 }
 
 
@@ -60,7 +85,7 @@ iptw_model <- function(data, frm, model, ...){
     class(out) <- c("lm")
 
   }else if(model == "logit"){
-    out <- survey::svyglm(frm, design = design, family = quasibinomial)
+    out <- survey::svyglm(frm, design = design, family = quasibinomial) # model
     class(out) <- c("glm")
 
   }else if(model == "cox"){
@@ -69,3 +94,5 @@ iptw_model <- function(data, frm, model, ...){
   }
   out
 }
+
+?survey::svycoxph
