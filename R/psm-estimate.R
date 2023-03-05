79bab528381582780b88d4d876780fcc39f6f763
KunHuo <610112077@qq.com>
2023-03-05 11:42:16 +0800

    Update psm-estimate.R

--- R/psm-estimate.R
+++ R/psm-estimate.R
@@ -2,7 +2,7 @@
 #'
 #' @param object a psmatch object; the output of a call to [psm()].
 #' @param model model regression.
-#' @param ... further armuments.
+#' @param ... further armuments pass to [typeset()].
 #'
 #' @return a data frame.
 #' @export
@@ -24,13 +24,25 @@ psm_estimate <- function(object, model = "auto", ...){
             covariates = covariates,
             model = model,
             ...)
-  res.adj[1, 1] <- "Adjusted model"
+  res.adj[1, 1] <- "Multivariable model a"
 
   frm <- create_formula(c(time, outcome), c(exposure))
 
   data.psm <- psm_data(object)
 
   if(model == "cox"){
+    fit.psm <- srmisc::do_call(model,
+                               data = data.psm,
+                               formula = frm,
+                               positive = positive,
+                               weights = data.psm$weights,
+                               cluster = data.psm$subclass)
+
+    res.psm <- srmisc::typeset(fit.psm,
+                               data = data.psm,
+                               outcome = outcome,
+                               varnames = exposure,
+                               filter = exposure, ...)
 
   }else{
     if(model == "linear"){
@@ -47,6 +59,7 @@ psm_estimate <- function(object, model = "auto", ...){
                                  weights = data.psm$weights)
       exp <- TRUE
     }
+
     res.psm <- lmtest::coeftest(fit.psm,
                                 vcov. = sandwich::vcovCL,
                                 cluster = data.psm$subclass)
@@ -55,24 +68,62 @@ psm_estimate <- function(object, model = "auto", ...){
                                outcome = outcome,
                                varnames = exposure,
                                filter = exposure,
-                               exp = exp)
-    res.psm[1, 1] <- "Propensity matching"
-    names(res.psm) <- names(res.adj)
+                               exp = exp, ...)
   }
+  res.psm[1, 1] <- "Propensity matching b"
+  names(res.psm) <- names(res.adj)
 
   data.iptw <- psm_data(object, type = "iptw")
-  data.iptw <- positive_event(data = data.iptw, outcome = outcome, positive)
+
+  if(length(unique(data.iptw[[outcome]])) == 2L){
+    data.iptw <- positive_event(data = data.iptw, outcome = outcome, positive)
+  }
   res.iptw <- iptw_model(data = data.iptw, frm = frm, model = model)
   res.iptw <- typeset(res.iptw,
                       data = data.iptw,
                       outcome = outcome,
                       varnames = exposure,
-                      filter = exposure)
-  res.iptw
-  # res.iptw[1, 1] <- "IPTW"
-  # out <- rbind(res.adj, res.psm)
-  # out <- rbind(out, res.iptw)
-  # out
+                      filter = exposure, ...)
+  res.iptw[1, 1] <- "IPTW c"
+  res.iptw[2, 2] <- "-"
+  res.iptw[3, 2] <- "-"
+  out <- rbind(res.adj, res.psm)
+  out <- rbind(out, res.iptw)
+  names(out)[1] <- "Models"
+
+  abbr <- switch(model,
+                 linear  = "Abbreviation: IPTW, inverse probability treatment weighting; CI, confidence interval.",
+                 logit   = "Abbreviation: IPTW, inverse probability treatment weighting; OR, odds ratio; CI, confidence interval.",
+                 poson   = "Abbreviation: IPTW, inverse probability treatment weighting; RR, risk ratio; CI, confidence interval.",
+                 logbinom = "Abbreviation: IPTW, inverse probability treatment weighting; RR, risk ratio; CI, confidence interval.",
+                 cox     = "Abbreviation: IPTW, inverse probability treatment weighting; HR, hazard ratio; CI, confidence interval.",
+                 "Abbreviation: IPTW, inverse probability treatment weighting; CI, confidence interval.")
+
+  adjusted <- paste(sapply(covariates, function(x){ srmisc::get_var_label(object$data, x, default = ".name") }),  collapse = ", ")
+  note1 <-sprintf("a Adjusted for %s before matching.", adjusted)
+  note2 <- "b Propensity score matching with a cluster-robust standard error."
+  note3 <- "c IPTW with a cluster-robust standard error."
+  notes <- paste(abbr, note1, note2, note3, sep = "\n")
+
+  label.exposure <- srmisc::get_var_label(object$data, exposure, default = ".name")
+  label.outcome  <- srmisc::get_var_label(object$data, outcome,  default = ".name")
+
+  title <- switch(model,
+                  linear  = "multiple linear regression",
+                  logit   = "binary logistc regression",
+                  poson   = "modified Poissson regression",
+                  logbinom = "log-binomial regression",
+                  cox     = "Cox proportional hazards regression",
+                  default = "")
+  title <- sprintf("Table: Association between %s and %s using %s",
+                   label.exposure,
+                   label.outcome,
+                   title)
+
+  attr(out, "title") <- title
+  attr(out, "note") <- notes
+
+  out
 }
 
 
@@ -85,7 +136,15 @@ iptw_model <- function(data, frm, model, ...){
     class(out) <- c("lm")
 
   }else if(model == "logit"){
-    out <- survey::svyglm(frm, design = design, family = quasibinomial) # model
+    out <- survey::svyglm(frm, design = design, family = stats::quasibinomial) # model
+    class(out) <- c("glm")
+
+  }else if(model == "poson"){
+    out <- survey::svyglm(frm, design = design, family = stats::poisson(link = "log")) # model
+    class(out) <- c("glm")
+
+  }else if(model == "logbinom"){
+    out <- survey::svyglm(frm, design = design, family = stats::binomial(link = "log")) # model
     class(out) <- c("glm")
 
   }else if(model == "cox"){
@@ -94,5 +153,3 @@ iptw_model <- function(data, frm, model, ...){
   }
   out
 }
-
-?survey::svycoxph
